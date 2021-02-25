7da2f96fd4d21dd122d66a1b555779c92998a522
Martin Najjar <martin_najjar@yahoo.com>
2021-02-26 02:25:34 +0300

    Implement the remove book button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { REMOVE_BOOK } from '../actions/index';
 
-const Book = ({ book }) => (
-  <>
-    <td>{ book.id }</td>
-    <td>{ book.title }</td>
-    <td>{ book.category }</td>
-  </>
-);
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <td>{ book.id }</td>
+      <td>{ book.title }</td>
+      <td>{ book.category }</td>
+      <td>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(REMOVE_BOOK(book));
+          }}
+        >
+          Remove Book
+        </button>
+      </td>
+    </>
+  );
+};
 
 Book.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -17,6 +17,7 @@ const BooksList = () => {
           <th>Book ID</th>
           <th>Title</th>
           <th>Category</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
