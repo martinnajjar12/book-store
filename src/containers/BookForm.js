05cb81e28de5c6f30477fbdd450b7d4209643b5c
Martin Najjar <martin_najjar@yahoo.com>
2021-02-26 01:16:53 +0300

    Create handleInputChange, handleSelectChange and dispatch function

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import shortid from 'shortid';
+import { CREATE_BOOK } from '../actions/index';
 
-const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+const categories = ['', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
 const BookForm = () => {
   const category = categories.map(category => (
     <option key={category} value={category}>{ category }</option>
   ));
+  const [inputValue, setInputValue] = useState('');
+  const [selectValue, setSelectValue] = useState('');
+
+  const handleInputChange = e => {
+    setInputValue(e.target.value);
+  };
+  const handleSelectChange = e => {
+    setSelectValue(e.target.value);
+  };
+  const dispatch = useDispatch();
+
   return (
     <>
       <form>
-        <input type="text" />
-        <select>
+        <input value={inputValue} onChange={handleInputChange} type="text" />
+        <select value={selectValue} onChange={handleSelectChange}>
           {category}
         </select>
-        <input type="submit" />
+        <button
+          onClick={() => {
+            dispatch(CREATE_BOOK({
+              id: shortid.generate(),
+              title: inputValue,
+              category: selectValue,
+            }));
+          }}
+          type="button"
+        >
+          Submit
+        </button>
       </form>
     </>
   );
